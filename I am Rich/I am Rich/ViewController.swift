08673eea440108da76386b5cd4d769bd07610dd4
Shaw Lu <shawlu95@gmail.com>
2023-06-07 09:19:10 -0700

    I am rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by main on 6/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
