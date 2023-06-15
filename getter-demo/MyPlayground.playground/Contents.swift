5059d5dc430626ea5669f8d70f463f4c1a088cf4
Shaw Lu <shawlu95@gmail.com>
2023-06-15 00:44:14 -0700

    computed property (like spreadsheet)

--- getter-demo/MyPlayground.playground/Contents.swift
+++ getter-demo/MyPlayground.playground/Contents.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 // stored properties
-let pizzaInInches: Int = 10
+let pizzaInInches: Int = 16
+var numberOfPeople: Int = 12
 
 // computed properties has to be var
-var numberOfPeople: Int {
-    print("Getter is called")
-    return pizzaInInches - 4 // this is a getter
-}
+//var numberOfPeople: Int {
+//    print("Getter is called")
+//    return pizzaInInches - 4 // this is a getter
+//}
 
 var numberOfSlices: Int {
     get {
@@ -24,5 +25,25 @@ print(numberOfPeople)
 let a = numberOfPeople * 2
 
 // call setter with asign operator
-numberOfSlices = 12
+numberOfSlices = 24
+
+let slicesPerPerson: Int = 4
+var numberOfPizza: Int {
+    get {
+        print(numberOfSlices)
+        print(slicesPerPerson)
+        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        return numberOfPeople / numberOfPeopleFedPerPizza
+    }
+    set {
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
+    }
+}
+
+print(numberOfPizza)
+
+numberOfPizza = 8
+print(numberOfPeople)
+
 
